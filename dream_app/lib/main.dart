ce11fdba383a11bc50d71b52c88189f8aa3ac509
Eshaan Anand <eshaananand2911@gmail.com>
2022-02-09 09:29:55 +0530

    dream-app Added
    FlutterVerse - Day-1 (Dream App Code)

---
+++ dream_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFE041562),
+          centerTitle: true,
+          title: (Text('Dream App')),
+        ),
+        body: Center(
+            child: Image(
+          image: AssetImage('images/car.jpg'),
+        )),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
