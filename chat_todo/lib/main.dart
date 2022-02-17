f1692593c3409529e20e16f172fb63e8881bb0ea
Subhayan Das <nips.subhayan@gmail.com>
2022-02-17 23:28:41 +0530

    Add files via upload

---
+++ chat_todo/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:chat_todo/Pages/home_page.dart';
+import 'package:chat_todo/Pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      builder: (context, snap) =>
+          FirebaseAuth.instance.currentUser == null ? LoginPage() : HomePage(),
+      stream: FirebaseAuth.instance.authStateChanges(),
+    );
+  }
+}
